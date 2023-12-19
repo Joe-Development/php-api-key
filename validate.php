015a7f0862313ca0be5cb55804a2441e96a918c2
Joe Development <146123038+Joe-Development@users.noreply.github.com>
2023-12-19 17:29:31 -0500

    Update validate.php

--- validate.php
+++ validate.php
@@ -1,51 +1,17 @@
 <?php
 
-$servername = "127.0.0.1:3308";
-$username = "joe";
-$password = "joev2";
-$dbname = "api";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die(json_encode(['status' => 'error', 'message' => 'Connection failed']));
-}
-
-function validateLicenseKey($licenseKey) {
-    global $conn;
-
-    $sql = "SELECT * FROM licenses WHERE license_key = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param('s', $licenseKey);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        $currentTimestamp = time();
-        $expirationTimestamp = strtotime($row['expiration_date']);
-
-        if ($currentTimestamp <= $expirationTimestamp) {
-            $timeLeftSeconds = $expirationTimestamp - $currentTimestamp;
-            $timeLeftDays = ceil($timeLeftSeconds / (60 * 60 * 24));
-            return ['status' => 'success', 'message' => 'License key is valid', 'time_left' => $timeLeftSeconds, 'time_left_days' => $timeLeftDays];
-        } else {
-            return ['status' => 'error', 'message' => 'License key has expired'];
-        }
-    } else {
-        return ['status' => 'error', 'message' => 'Invalid license key'];
-    }
-}
+include('config.php');
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    if (isset($_GET['license'])) {
+    if (isset($_GET['license']) && isset($_GET['username'])) {
         $licenseKey = $_GET['license'];
-        $result = validateLicenseKey($licenseKey);
+        $username = $_GET['username'];
+        $result = validateLicenseAndUsername($licenseKey, $username);
         header('Content-Type: application/json');
         echo json_encode($result);
     } else {
-        $response = ['status' => 'error', 'message' => 'License key not provided in the URL'];
+        $response = ['status' => 'error', 'message' => 'License key or username not provided in the URL'];
         header('Content-Type: application/json');
         echo json_encode($response);
     }
@@ -54,6 +20,40 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     header('Content-Type: application/json');
     echo json_encode($response);
 }
+
+function validateLicenseAndUsername($licenseKey, $username) {
+    global $conn;
+
+    $selectSql = "SELECT * FROM licenses WHERE license_key = ? AND username = ?";
+    $selectStmt = $conn->prepare($selectSql);
+    $selectStmt->bind_param('ss', $licenseKey, $username);
+    $selectStmt->execute();
+    $selectResult = $selectStmt->get_result();
+
+    if ($selectResult->num_rows > 0) {
+        $row = $selectResult->fetch_assoc();
+        $currentTimestamp = time();
+        $expirationTimestamp = strtotime($row['expiration_date']);
+
+        if ($currentTimestamp <= $expirationTimestamp) {
+            $timeLeftSeconds = $expirationTimestamp - $currentTimestamp;
+            $timeLeftDays = ceil($timeLeftSeconds / (60 * 60 * 24));
+            return ['status' => 'success', 'message' => 'License key is valid', 'time_left_days' => $timeLeftDays];
+        } else {
+            $deleteSql = "DELETE FROM licenses WHERE license_key = ?";
+            $deleteStmt = $conn->prepare($deleteSql);
+            $deleteStmt->bind_param('s', $licenseKey);
+            $deleteStmt->execute();
+
+            return ['status' => 'error', 'message' => 'License key has expired and deleted'];
+        }
+    } else {
+        return ['status' => 'error', 'message' => 'Invalid license key or username'];
+    }
+}
+
+
+
 
 $conn->close();
 
