1826d32ddede82e1e8da3f928e3102619a551514
Joe Development <146123038+Joe-Development@users.noreply.github.com>
2023-12-19 17:30:57 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,5 +1,6 @@
 CREATE TABLE licenses (
     id INT AUTO_INCREMENT PRIMARY KEY,
     license_key VARCHAR(255) NOT NULL,
-    expiration_date TIMESTAMP NOT NULL
+    expiration_date TIMESTAMP NOT NULL,
+    username VARCHAR(255) NOT NULL
 );
